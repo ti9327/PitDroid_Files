918e0b6ae7f90977bfc4a33098d7e7a689524ce2
David Castellano <davec3275@gmail.com>
2025-02-01 15:46:38 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PitDroid_Files
+
+There are the files I modifed for use with my animated Pit Droid with 14 points of articulation.  
---
+++ Sketches/Default/BottangoArduinoDriver/BottangoArduinoDriver.ino
@@ -0,0 +1,15 @@
+// !!! DRIVER VERSION: 0.7.0a1 !!!
+// !!! Api Version: 8 !!!
+
+#include "src/BottangoCore.h"
+#include "src/BasicCommands.h"
+
+void setup()
+{
+  BottangoCore::bottangoSetup();
+}
+
+void loop()
+{
+  BottangoCore::bottangoLoop();
+}
